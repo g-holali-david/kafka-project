5ec899c4dfac65c0157d9b353af076a7e5b7f672
GAVI Holali David 00077501 <holalidavid.gavi@3ds.com>
2024-12-16 17:57:54 +0100

    Testing services 3

--- kafka-streams/src/main/java/com/nfe101/kafka_streams/services/KafkaStreamsService.java
+++ kafka-streams/src/main/java/com/nfe101/kafka_streams/services/KafkaStreamsService.java
@@ -11,7 +11,7 @@ import org.apache.kafka.streams.kstream.KTable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.util.Properties;
 
 @Service
